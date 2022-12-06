2407a7547f6f8c3c8db82a5d136523aa29e69f20
Sunny <70151698+kishorekumar300@users.noreply.github.com>
2022-12-07 00:47:15 +0530

    Add files via upload

---
+++ load.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "admin.h"
+
+using namespace std;
+
+void start_load(void);
+void end_load(void);
+void print_label(int n);
+void load_view(int);
---
+++ sdm.cpp
@@ -0,0 +1,10 @@
+#include"admin.h"
+#include"load.h"
+
+int main()
+{
+	start_load();
+	Admin admin; 
+	end_load();
+	return 0;
+}
